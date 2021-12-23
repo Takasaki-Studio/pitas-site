427a1a112cf8bbf3df6f0c421fdd3bf04656e447
Takasakiii <lucasmc2709@live.com>
2021-12-22 23:20:35 -0300

    basic database connection

---
+++ core/migrations/000_test.sql
@@ -0,0 +1,3 @@
+create table contador (
+    valor integer not null default 0
+)
--- core/src/main.rs
+++ core/src/main.rs
@@ -1,16 +1,32 @@
+mod database;
+
 #[macro_use]
 extern crate rocket;
 
+use database::DatabasePool;
 use rocket::fs::FileServer;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[get("/counter")]
+async fn counter(pool: &rocket::State<DatabasePool>) -> String {
+    sqlx::query("insert into contador values (1)")
+        .execute(&**pool)
+        .await
+        .unwrap();
+
+    let counter: (i64,) = sqlx::query_as("select count(*) from contador")
+        .fetch_one(&**pool)
+        .await
+        .unwrap();
+
+    counter.0.to_string()
 }
 
 #[launch]
-fn run() -> _ {
+async fn run() -> _ {
+    let db_pool = database::connect_database().await;
+
     rocket::build()
-        .mount("/test", routes![index])
+        .manage::<DatabasePool>(db_pool)
         .mount("/", FileServer::from("static"))
+        .mount("/", routes![counter])
 }
