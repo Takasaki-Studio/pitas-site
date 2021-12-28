26ad2fa2bac388c28389f50a0524b32059373062
Takasakiii <lucasmc2709@live.com>
2021-12-27 22:43:40 -0300

    list videos

---
+++ core/src/bot/commands/listvideos.ts
@@ -0,0 +1,39 @@
+import { Command } from "../handler";
+
+import { list } from "../../videos-manager";
+
+const listVideos: Command = {
+  name: "listvideos",
+  cmdOptions(builder) {
+    return builder
+      .setDescription("List all videos")
+      .addNumberOption((option) =>
+        option.setName("page").setDescription("The page number")
+      );
+  },
+  async execute(interaction) {
+    const page = interaction.options.getNumber("page") || 1;
+
+    const videos = await list(page);
+    const formatedVideos = videos.videos
+      .map((video) =>
+        video.length > 30 ? `${video.substring(0, 30)}...` : video
+      )
+      .join("\n");
+    interaction.reply({
+      embeds: [
+        {
+          title: "Videos",
+          description: formatedVideos,
+          color: 53380,
+          footer: {
+            text: `Page ${page} of ${Math.ceil(videos.total / 10)}`,
+          },
+        },
+      ],
+      ephemeral: true,
+    });
+  },
+};
+
+export default listVideos;
--- core/src/videos-manager.ts
+++ core/src/videos-manager.ts
@@ -1,11 +1,22 @@
 import { create } from "youtube-dl-exec";
 import path from "path";
+import fs from "fs/promises";
+
+const videoFolder = path.join(__dirname, "..", "videos");
+const ytdl = create(path.join(__dirname, "..", "bin", "yt-dlp"));
 
 export async function download(url: string) {
-  const ytdl = create(path.join(__dirname, "..", "bin", "yt-dlp"));
-
   await ytdl(url, {
     format: "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
     output: "videos/%(title)s.%(ext)s",
   });
+}
+
+export async function list(page = 1) {
+  const videos = await fs.readdir(videoFolder);
+  const videosFormated = videos.slice((page - 1) * 10, page * 10);
+  return {
+    videos: videosFormated,
+    total: videos.length,
+  };
 }
