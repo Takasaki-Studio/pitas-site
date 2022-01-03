8be381c8345e49f2b02f528e548a3766747d40b0
KuryKat <57152799+KuryKat@users.noreply.github.com>
2022-01-03 17:29:09 -0300

    p r e t t i e r

--- frontend/src/system/root/eval.tsx
+++ frontend/src/system/root/eval.tsx
@@ -1,24 +1,20 @@
 import { Execultor } from "../shell";
 
 export interface LinkProps {
-  code: string
+  code: string;
 }
 
-function Eval (
-  props: LinkProps
-) {
-  return (
-    <div dangerouslySetInnerHTML={{__html: props.code}} />
-  )
+function Eval(props: LinkProps) {
+  return <div dangerouslySetInnerHTML={{ __html: props.code }} />;
 }
 
 const evalCommand: Execultor = (args, cli) => {
-  const htmlCode = args.join(" ")
+  const htmlCode = args.join(" ");
 
   cli.xserver({
     title: document.title,
-    content: <Eval code={htmlCode} />
-  })
+    content: <Eval code={htmlCode} />,
+  });
 };
 
 export default evalCommand;
